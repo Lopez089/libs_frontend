d344c699de59dad2c341d1d52d6c29faf7a230b5
lopez089 <aragonlopezjuan87@gmail.com>
2017-01-12 16:03:30 +0100

    actualizaciones

--- js/libs.js
+++ js/libs.js
@@ -1,1 +1,22 @@
-/*alert("funciona");*/
+/* MENU */
+	/*function MenuTrasicion(px,gama,color,tiempo) {*/
+		
+		$('a')
+		.mouseover(function(){
+			$("a").stop().animate(
+				{borderBottomColor:"#434"}, 
+				{duration:600})
+		console.log("entrada esta bien");
+		})
+		.mouseout(function(){
+			$(".MenuTrasicion").stop().animate(
+				{"border-bottom-color":"#668"}, 
+				{duration:600})
+			console.log("salida esta bien");
+			})
+/*alert("todo esta bien");*/
+
+
+
+
+		
